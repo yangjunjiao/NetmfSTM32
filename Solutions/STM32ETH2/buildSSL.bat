542476f7ca3f66bf6ea4232bc22125aaeba4e591
Stefan <Stefan.Kraemer@hs-regensburg.de>
2012-05-30 17:05:25 +0200

    added eclipse workspace

--- Solutions/STM32ETH2/buildSSL.bat
+++ Solutions/STM32ETH2/buildSSL.bat
@@ -1,1 +1,8 @@
+
+
+
+cd D:\MicroFrameworkPK_v4_2
+setenv_MDK4.12.cmd C:\Keil\ARM
+cd D:\MicroFrameworkPK_v4_2\Solutions\STM32ETH2
+
 msbuild dotNetMF.proj /t:build /p:flavor=debug;TCP_IP_STACK=LWIP;memory=FLASH;platform=STM32ETH2;SSL=OPEN_SSL
